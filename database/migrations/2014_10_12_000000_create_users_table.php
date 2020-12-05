5711c9ce3273d18f89606fde05ae07997f6a52d0
tomfarrelly <tomfarrelly20@gmail.com>
2020-12-05 21:12:18 +0000

    user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,4 +1,11 @@
 <?php
+# @Author: tomfarrelly
+# @Date:   2020-10-30T15:07:53+00:00
+# @Last modified by:   tomfarrelly
+# @Last modified time: 2020-12-05T21:11:53+00:00
+
+
+
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -17,8 +24,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('bio');
+            $table->string('genre');
+            $table->string('location');
             $table->rememberToken();
             $table->timestamps();
         });
