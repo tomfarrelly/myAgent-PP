f716dbf581e40d76d3e92f162b624497f1688bed
DawidKarcz <bebos2000@gmail.com>
2020-12-08 20:05:31 +0000

    Not working

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\Dj\EventController as DjEventController;
-use App\Http\Controllers\EventManager\EventController as EventManagerEventController;
-use App\Http\Controllers\Admin\EventController as AdminEventController;
-
-use App\Http\Controllers\Dj\HomeController as DjHomeController;
-use App\Http\Controllers\EventManager\HomeController as EventManagerHomeController;
-use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,27 +13,13 @@ use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 |
 */
 
-
-
-Route::get('/', [PageController::class, 'welcome'])->name('welcome');
-Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin.home');
-Route::get('/dj/home', [DjHomeController::class, 'index'])->name('dj.home');
-Route::get('/eventmanager/home', [EventManagerHomeController::class, 'index'])->name('eventmanager.home');
-
-
-// Route::get('/user/books', [UserBookController::class, 'index'])->name('user.books.index');
-// Route::get('/user/books/{id}', [UserBookController::class, 'show'])->name('user.books.show');
-//
-// Route::get('/admin/books', [AdminBookController::class, 'index'])->name('admin.books.index');
-// Route::get('/admin/books/create', [AdminBookController::class, 'create'])->name('admin.books.create');
-// Route::get('/admin/books/{id}', [AdminBookController::class, 'show'])->name('admin.books.show');
-// Route::post('/admin/books/store', [AdminBookController::class, 'store'])->name('admin.books.store');
-// Route::get('/admin/books/{id}/edit', [AdminBookController::class, 'edit'])->name('admin.books.edit');
-// Route::put('/admin/books/{id}', [AdminBookController::class, 'update'])->name('admin.books.update');
-// Route::delete('/admin/books/{id}', [AdminBookController::class, 'destroy'])->name('admin.books.destroy');
+Route::get('/admin/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
+Route::get('/dj/home', [App\Http\Controllers\Dj\HomeController::class, 'index'])->name('dj.home');
+Route::get('/eventmanager/home', [App\Http\Controllers\EventManager\HomeController::class, 'index'])->name('eventmanager.home');
